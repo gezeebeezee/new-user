7bf1011398637a23cba52c4500a4461775f7c083
Hasan Syed <syed_hasan@bah.com>
2023-03-09 23:20:17 -0500

    Added functionality

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ app.use(express.json());
 const mongoose = require("mongoose");
 const cors = require("cors");
 app.use(cors());
+const bcrypt = require("bcryptjs");
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET =
+  "hvdvay6ert72839289()aiyg8t87qt72393293883uhefiuh78ttq3ifi78272jbkj?[]]pou89ywe";
 
 const mongoUrl =
   "mongodb+srv://h1syed:cs361@cluster0.s8dap6k.mongodb.net/?retryWrites=true&w=majority";
@@ -52,4 +60,50 @@ app.post("/register", async (req, res) => {
   } catch (error) {
     res.send({ status: "error" });
   }
+});
+
+app.post("/login-user", async (req, res) => {
+  const { userName, password } = req.body;
+
+  const user = await User.findOne({ userName });
+  if (!user) {
+    return res.json({ error: "User Not found" });
+  }
+  if (await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign({ userName: user.userName }, JWT_SECRET, {
+      expiresIn: "15m",
+    });
+
+    if (res.status(201)) {
+      return res.json({ status: "ok", data: token });
+    } else {
+      return res.json({ error: "error" });
+    }
+  }
+  res.json({ status: "error", error: "Invalid Password" });
+});
+
+app.post("/userDetails", async (req, res) => {
+  const { token } = req.body;
+  try {
+    const user = jwt.verify(token, JWT_SECRET, (err, res) => {
+      if (err) {
+        return "token expired";
+      }
+      return res;
+    });
+    console.log(user);
+    if (user === "token expired") {
+      return res.send({ status: "error", data: "token expired" });
+    }
+
+    const useremail = user.email;
+    User.findOne({ email: useremail })
+      .then((data) => {
+        res.send({ status: "ok", data: data });
+      })
+      .catch((error) => {
+        res.send({ status: "error", data: error });
+      });
+  } catch (error) {}
 });
